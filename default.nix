b4c5e6c31335f98e093aca806aebea5486c655d7
jcleng <j.c.leng@foxmail.com>
2021-01-05 14:05:43 +0800

    mariadb-10.5.8

--- default.nix
+++ default.nix
@@ -1,7 +1,9 @@
 with import <nixpkgs> {}; stdenv.mkDerivation {
-  # 参考 https://gitee.com/jcleng/doc/blob/master/docs/linux下用cmake编译安装mysql.md
-  # aria2c http://dev.mysql.com/get/Downloads/MySQL-5.6/mysql-5.6.35.tar.gz
-  name = "mysql-5.6.35";
+  # 下载源码包,解压,移动到src目录
+  # aria2c https://mirrors.tuna.tsinghua.edu.cn/mariadb/mariadb-10.5.8/source/mariadb-10.5.8.tar.gz
+  # tar xvf mariadb-10.5.8.tar.gz
+  # mv mariadb-10.5.8/ ./src
+  name = "mariadb-10.5.8";
 
   # Source Code
   # See: https://nixos.org/nixpkgs/manual/#ssec-unpack-phase
@@ -9,7 +11,7 @@ with import <nixpkgs> {}; stdenv.mkDerivation {
 
   # Dependencies
   # See: https://nixos.org/nixpkgs/manual/#ssec-stdenv-dependencies
-  buildInputs = [ coreutils gcc cmake ncurses ];
+  buildInputs = [ coreutils gcc cmake ncurses bison gnutls bzip2 libaio];
 
   # Build Phases
   # See: https://nixos.org/nixpkgs/manual/#sec-stdenv-phases
@@ -21,9 +23,9 @@ with import <nixpkgs> {}; stdenv.mkDerivation {
     # 编译错误再次编译需要删除该文件
     rm -rf CMakeCache.txt
     cmake \
--DCMAKE_INSTALL_PREFIX=$out/mysql56 \
--DMYSQL_DATADIR=$out/mysql56 \
--DSYSCONFDIR=$out/mysql56/etc \
+-DCMAKE_INSTALL_PREFIX=$out/mariadb1058 \
+-DMYSQL_DATADIR=$out/mariadb1058 \
+-DSYSCONFDIR=$out/mariadb1058/etc \
 -DWITH_MYISAM_STORAGE_ENGINE=1 \
 -DWITH_INNOBASE_STORAGE_ENGINE=1 \
 -DWITH_ARCHIVE_STORAGE_ENGINE=1 \
@@ -33,7 +35,7 @@ with import <nixpkgs> {}; stdenv.mkDerivation {
 -DDEFAULT_COLLATION=utf8_general_ci \
 -DEXTRA_CHARSETS=all \
 -DMYSQL_TCP_PORT=3306 \
--DMYSQL_UNIX_ADDR=$out/mysqld.sock \
+-DMYSQL_UNIX_ADDR=/tmp/mariadb1058.sock \
 -DWITH_DEBUG=0
     make -j8
   '';
